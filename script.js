e98559451ff131e92ae1657b308a2c451e54dfc4
Pooja <poojasri.s@skematix.in>
2025-12-18 01:21:35 +0530

    update

---
+++ script.js
@@ -0,0 +1,52 @@
+// Donut chart add data and config
+
+const chart = document.getElementById("myChart");
+
+// ✅ Category-wise data
+const data = {
+  labels: ["Food", "Travel", "Medicine", "Shopping", "Bills"],
+  datasets: [
+    {
+      label: "Expenses by Category",
+      data: [2500, 1800, 1200, 2200, 1500], // example amounts
+      backgroundColor: [
+        "#f97316", // Food - orange
+        "#38bdf8", // Travel - sky blue
+        "#22c55e", // Medicine - green
+        "#a855f7", // Shopping - purple
+        "#ef4444", // Bills - red
+      ],
+      borderWidth: 1,
+       hoverOffset: 4
+    },
+  ],
+};
+
+// // ✅ Config AFTER data
+const config = {
+  type: "doughnut",
+  data: data,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: {
+          color: "#374151", // gray-700
+          font: {
+            size: 14,
+            weight: "600",
+          },
+        },
+      },
+      title: {
+        display: true,
+        text: "Income vs Expense",
+      },
+    },
+  },
+};
+
+// ✅ Create chart
+new Chart(chart, config);
