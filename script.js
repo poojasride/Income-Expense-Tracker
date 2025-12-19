2101645680696152c4e066b49e63d76cf1d2b20d
Pooja <poojasri.s@skematix.in>
2025-12-19 13:40:02 +0530

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const data = {
   datasets: [
     {
       label: "Expenses by Category",
-      data: [2500, 1800, 1200, 2200, 1500], // example amounts
+      data: [2500, 1800, 1200, 2200, 1500],
       backgroundColor: [
         "#f97316", // Food - orange
         "#38bdf8", // Travel - sky blue
@@ -43,7 +43,7 @@ const config = {
       title: {
         display: true,
         text: "Spending overview",
-        color: "#4d525bff", 
+        color: "#4d525bff",
         font: {
           size: 16,
           weight: "600",
@@ -58,3 +58,63 @@ const config = {
 
 // ✅ Create chart
 new Chart(chart, config);
+
+//Get Transactions
+
+async function getTransctions() {
+  const url = "https://6944a75e7dd335f4c360d98f.mockapi.io/transaction";
+  try {
+    const response = await fetch(url);
+
+    if (!response) throw new Error(`Response : ${response.status} `);
+
+    const transactions = await response.json();
+
+    // Sum income and formatted  $0,000 USD currency formated
+    let totalIncome = await calculateIncome(transactions);
+    let formattedIncome = await formattedNumber(totalIncome);
+
+    //Sum of expense and formatted  $0,000 USD currency formated
+    let totalExpense = await calculateExpense(transactions);
+    let formattedExpense = await formattedNumber(totalExpense);
+
+    //sum of balance and formatted  $0,000 USD currency formated
+    let balance = totalIncome - totalExpense;
+    let formattedBal = await formattedNumber(balance);
+
+    // Handled Overview Cards  Income  &Expense &Balance
+    document.getElementById("income").innerHTML = `+ ${formattedIncome}`;
+    document.getElementById("expense").innerHTML = `- ${formattedExpense}`;
+    document.getElementById("balance").innerHTML = `- ${formattedBal}`;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getTransctions();
+
+async function calculateIncome(transactions) {
+  return transactions
+    .filter((transaction) => transaction.type === "Income")
+    .reduce((sum, transaction) => sum + transaction.amount, 0);
+}
+
+async function calculateExpense(transactions) {
+  return transactions
+    .filter((transaction) => transaction.type === "Expense")
+
+    .reduce((sum, transaction) => sum + transaction.amount, 0);
+}
+
+async function formattedNumber(amount) {
+  // Format for US Dollar currency
+  const formattedNumber = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0, // Set to 0 avoid decimal
+    maximumFractionDigits: 0,
+  }).format(amount);
+
+  return formattedNumber;
+}
