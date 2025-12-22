b5f666276ca95bac9ce2062cc85e26f21fd7b259
Pooja <poojasri.s@skematix.in>
2025-12-22 09:38:58 +0530

    update

--- script.js
+++ script.js
@@ -1,68 +1,80 @@
 // Donut chart add data and config
+async function donutChart(categoryTotal) {
+  const catLabels = Object.keys(categoryTotal);
+  const catValues = Object.values(categoryTotal);
 
-const chart = document.getElementById("myChart");
+  function dynamicColors() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
 
-// ✅ Category-wise data
-const data = {
-  labels: ["Food", "Travel", "Medicine", "Shopping", "Bills"],
-  datasets: [
-    {
-      label: "Expenses by Category",
-      data: [2500, 1800, 1200, 2200, 1500],
-      backgroundColor: [
-        "#f97316", // Food - orange
-        "#38bdf8", // Travel - sky blue
-        "#22c55e", // Medicine - green
-        "#a855f7", // Shopping - purple
-        "#ef4444", // Bills - red
-      ],
-      borderWidth: 1,
-      hoverOffset: 2,
-    },
-  ],
-};
+  // Array to store dynamic colors
+  const backgroundColors = [];
 
-// // ✅ Config AFTER data
-const config = {
-  type: "doughnut",
-  data: data,
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        position: "right",
-        labels: {
-          color: "#374151", // gray-700
+  // Loop through your data points and assign a random color for each
+  for (let i = 0; i < catLabels.length; i++) {
+    backgroundColors.push(dynamicColors()); // Use the RGB function defined above
+  }
+
+  const chart = document.getElementById("myChart");
+
+  // ✅ Category-wise data
+  const data = {
+    labels: catLabels,
+    datasets: [
+      {
+        label: "amount", // this label for hover on chart
+        data: catValues,
+        backgroundColor: backgroundColors,
+        borderWidth: 1,
+        hoverOffset: 2,
+      },
+    ],
+  };
+
+  // // ✅ Config AFTER data
+  const config = {
+    type: "doughnut",
+    data: data,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          position: "bottom",
+          labels: {
+            color: "#374151", // gray-700
+            font: {
+              size: 14,
+              weight: "600",
+            },
+          },
+        },
+        title: {
+          display: true,
+          text: "Income & Expence Spending Overview",
+          color: "#4d525bff",
           font: {
-            size: 14,
+            size: 16,
             weight: "600",
+          },
+          padding: {
+            bottom: 10,
           },
         },
       },
-      title: {
-        display: true,
-        text: "Spending overview",
-        color: "#4d525bff",
-        font: {
-          size: 16,
-          weight: "600",
-        },
-        padding: {
-          bottom: 10,
-        },
-      },
     },
-  },
-};
+  };
 
-// ✅ Create chart
-new Chart(chart, config);
+  // ✅ Create chart
+  new Chart(chart, config);
+}
 
 //Get Transactions
-
-async function getTransctions() {
-  const url = "https://6944a75e7dd335f4c360d98f.mockapi.io/transaction";
+async function getTransactions() {
+  const url = "https://6944a75e7dd335f4c360d98f.mockapi.io/transaction-entry";
   try {
     const response = await fetch(url);
 
@@ -87,12 +99,11 @@ async function getTransctions() {
     document.getElementById("expense").innerHTML = `- ${formattedExpense}`;
     document.getElementById("balance").innerHTML = `- ${formattedBal}`;
 
+    return transactions;
   } catch (error) {
     console.error(error);
   }
 }
-
-getTransctions();
 
 async function calculateIncome(transactions) {
   return transactions
@@ -118,3 +129,194 @@ async function formattedNumber(amount) {
 
   return formattedNumber;
 }
+
+function formatDate(timestamp) {
+  return new Date(timestamp * 1000)
+    .toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    })
+    .replace(/ /g, "-");
+}
+
+async function setDataForTable(transaction) {
+  const tableBody = document.getElementById("transaction-body");
+
+  tableBody.innerHTML = "";
+
+  transaction.forEach((item) => {
+    const row = document.createElement("tr");
+    row.className = "hover:bg-cyan-50 transition";
+
+    const descTd = document.createElement("td");
+    descTd.className = "px-6 py-3";
+    descTd.textContent = item.description;
+
+    const amountTd = document.createElement("td");
+    amountTd.className =
+      "px-6 py-3 font-medium " +
+      (item.type === "Expense" ? "text-red-500" : "text-green-600");
+    amountTd.textContent = `$${item.amount}`;
+
+    const categoryTd = document.createElement("td");
+    categoryTd.className = "px-6 py-3";
+    categoryTd.textContent = item.category;
+
+    const typeTd = document.createElement("td");
+    typeTd.className = "px-6 py-3";
+    typeTd.textContent = item.type;
+
+    const dateTd = document.createElement("td");
+    dateTd.className = "px-6 py-3";
+    dateTd.textContent = formatDate(item.date);
+
+    // ✅ ACTION BUTTONS TD
+    const actionTd = document.createElement("td");
+    actionTd.className = "px-3 py-1 text-center space-x-2";
+
+    // Edit button
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.className =
+      "px-3 py-1 text-sm border rounded text-blue-600 hover:bg-blue-50";
+
+    editBtn.addEventListener("click", () => {
+      console.log("Edit clicked for ID:", item.id);
+      // openEditModal(item);
+    });
+
+    // Delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className =
+      "px-3 py-1 text-sm border rounded text-red-600 hover:bg-red-50";
+
+    deleteBtn.addEventListener("click", () => {
+      console.log("Delete clicked for ID:", item.id);
+      // deleteTransaction(item.id);
+    });
+
+    actionTd.append(editBtn, deleteBtn);
+
+    row.append(descTd, amountTd, categoryTd, typeTd, dateTd, actionTd);
+    tableBody.appendChild(row);
+  });
+}
+
+//Add transaction  entry
+async function addTransaction(data) {
+  try {
+    const url = "https://6944a75e7dd335f4c360d98f.mockapi.io/transaction-entry";
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Handle the data for create transaction
+
+let selectedType = "Income"; // default
+
+const incomeBtn = document.getElementById("addTypeIncome");
+const expenseButton = document.getElementById("addTypeExpense");
+
+incomeBtn.addEventListener("click", () => {
+  selectedType = "Income";
+
+  incomeBtn.style.backgroundColor = "#22D3EE";
+  incomeBtn.style.color = "white";
+
+  expenseButton.style.backgroundColor = "#E5E7EB";
+  expenseButton.style.color = "black";
+});
+
+expenseButton.addEventListener("click", function () {
+  expenseButton.style.backgroundColor = "#22D3EE";
+  expenseButton.style.color = "white";
+
+  incomeBtn.style.backgroundColor = "#E5E7EB";
+  incomeBtn.style.color = "black";
+
+  selectedType = "Expense";
+});
+
+document.getElementById("addTypeExpense").addEventListener("click", () => {});
+
+const addEntry = document.getElementById("addEntry");
+
+addEntry.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const amount = document.getElementById("amount").value;
+  const category = document.getElementById("category").value;
+  const description = document.getElementById("description").value;
+
+  const data = {
+    amount: Number(amount), // convert to number
+    category: category,
+    description: description,
+    type: selectedType, // Income or Expense
+    date: new Date().toISOString(), // better format
+  };
+
+  console.log("Sending:", data);
+
+  addTransaction(data);
+});
+
+const resetBtn = document.getElementById("reset");
+
+resetBtn.addEventListener("click", resetData);
+
+const resetAddEntry = document.getElementById("resetEntry");
+
+resetAddEntry.addEventListener("click", resetData);
+
+function resetData() {
+  document.getElementById("amount").value = "";
+  document.getElementById("category").value = "";
+  document.getElementById("description").value = "";
+
+  incomeBtn.style.backgroundColor = "#22D3EE";
+  incomeBtn.style.color = "white";
+
+  expenseButton.style.backgroundColor = "#E5E7EB";
+  expenseButton.style.color = "black";
+}
+
+async function startFun() {
+  const transactions = await getTransactions();
+  console.log(`transactions : ${transactions}`);
+
+  const categoryTotal = transactions.reduce((acc, item) => {
+    const category = item.category.trim();
+    const amount = Number(item.amount);
+
+    if (!acc[category]) {
+      acc[category] = 0;
+    }
+
+    acc[category] += amount;
+    return acc;
+  }, {});
+
+  //This fun handle donut chart data and colors
+  donutChart(categoryTotal);
+
+  // This fun handle transaction history
+  setDataForTable(transactions);
+
+  console.log(categoryTotal);
+}
+
+startFun();
